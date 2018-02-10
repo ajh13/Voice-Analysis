9bb5f3a317f8668e845f2ec9b8f25ae8b3e870d4
Evan <evanksano@gmail.com>
2018-02-10 10:26:14 -0800

    Creating a connection module to handle connections to the DB, dependency - Will place unit testing for connection here - adding psycopg2 dependency for use in connecting to db - updated server.config to connect to a dev aws db

---
+++ Database/Connection.py
@@ -0,0 +1,68 @@
+import xml.etree.ElementTree as ET
+import psycopg2
+
+
+configPath = '../Dependencies/Database/server.config'
+'''
+Function: Reads server config file and converts xml to dictionary
+Args:
+          @filename: string path leading to server.config file
+Return:   containing the keys 'address', 'username', 
+          'password'(optional)
+'''
+def GetCredentials(filename):
+  credentials = {}
+  tree = ET.parse(filename)
+  root = tree.getroot()
+  for child in root:
+    credentials[child.tag] = child.text
+  return credentials
+
+
+'''
+Function: Opens connection to database using credentials passed to function
+          and creates a cursor objeect to be used to execute db operations
+Args:     
+          @filename: string path leading to server.config file
+Return:   cursor psycopg2 object (Used to perform database operations) 
+'''
+def Connect(filename):
+  credentials = GetCredentials(filename)
+  try:
+    conn = psycopg2.connect(dbname=credentials['dbname'],
+      user=credentials['username'],
+      password=credentials['password'],
+      host=credentials['host'])
+    cur = conn.cursor()
+  except:
+    print('Couldn\'t connect to server: ', credentials['host'])
+    exit() 
+  return (cur, conn)
+
+
+'''
+Function: Closes connection and cursor instances to db
+Args:     None
+Return:   Boolean on whether or not both cursor and connection were
+          successfully closed
+'''
+def Disconnect(connection, cursor):
+  success = True
+  if(connection):
+    connection.close()
+    print("Closing connection.")
+  else:
+    success = False
+    print("[Warning] Could not close connection. No connection to db found.")
+  if(cursor):
+    cursor.close()
+    print("Closing cursor.")
+  else:
+    success = False
+    print("[Warning] Could not close cursor. No cursor found.\n")
+  return success
+  
+def Main():
+  (curs, con)=Connect(configPath)
+  Disconnect(con,curs)
+Main()
