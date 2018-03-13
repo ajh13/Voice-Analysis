d1cb2687965dc21cc89b0496b4950c7e2ab3eecc
Alex Hall <a_hall11@u.pacific.edu>
2018-03-13 15:17:54 -0700

    Added InsertFromCSV this function will add to the trainingdata database if it is not already there based on a csv file

--- Database/Ingest.py
+++ Database/Ingest.py
@@ -1,3 +1,27 @@
-import psycopg2
+from Connection import Connect, ListTables, Disconnect, GrabFile
+import csv
 
-def CreateCursor():
+configPath = 'Dependencies/server.config'
+
+def InsertFromCSV(cursor, conn, filePath):
+    with open(filePath) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            sqlStatement = "SELECT filename FROM trainingdata WHERE filename = '" + row['filename'] + "'"
+            cursor.execute(sqlStatement)
+            sql = cursor.fetchone()
+            if sql is None:
+                sqlText = row['text']
+                sqlText = sqlText.replace("'","")
+                sqlInsert = "INSERT INTO trainingdata VALUES (%s, '%s', '%s', '%s', %s, '%s', '%s');" %\
+                    (row['up_votes'],sqlText,row['gender'],row['filename'],row['down_votes'],row['age'],row['accent'])
+                sqlInsert = sqlInsert.replace("''","NULL")
+                print(sqlInsert)
+                cursor.execute(sqlInsert)
+                conn.commit()
+
+def Main():
+    (curs, con) = Connect(configPath)
+    InsertFromCSV(curs,con,'Files/cv-valid-train.csv')
+    Disconnect(con,curs)
+Main()
