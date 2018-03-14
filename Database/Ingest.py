85eb11121b3aeed8a4ea649b0b7a97fdb6b731c2
Alex Hall <a_hall11@u.pacific.edu>
2018-03-13 17:49:24 -0700

    Added quick query add for fresh files, and slower one that checks duplicates

--- Database/Ingest.py
+++ Database/Ingest.py
@@ -1,11 +1,11 @@
-from Connection import Connect, ListTables, Disconnect, GrabFile
+from Connection import Connect, Disconnect
 import csv
 
 configPath = 'Dependencies/server.config'
 
-def InsertFromCSV(cursor, conn, filePath):
+def InsertFromCSVCheckForDuplicate(cursor, conn, filePath):
+    i = 0
     with open(filePath) as csvfile:
-        reader = csv.DictReader(csvfile)
         for row in reader:
             sqlStatement = "SELECT filename FROM trainingdata WHERE filename = '" + row['filename'] + "'"
             cursor.execute(sqlStatement)
@@ -20,8 +20,25 @@ def InsertFromCSV(cursor, conn, filePath):
                 cursor.execute(sqlInsert)
                 conn.commit()
 
-def Main():
-    (curs, con) = Connect(configPath)
-    InsertFromCSV(curs,con,'Files/cv-valid-train.csv')
-    Disconnect(con,curs)
-Main()
+def InsertFromCSVFRESH(cursor, conn, filePath):
+    i = 0
+    with open(filePath) as csvfile:
+        sqlInsert = ""
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            sqlText = row['text']
+            sqlText = sqlText.replace("'", "")
+            sqlInsert += "INSERT INTO trainingdata VALUES (%s, '%s', '%s', '%s', %s, '%s', '%s');" % \
+                        (row['up_votes'], sqlText, row['gender'], row['filename'], row['down_votes'], row['age'],
+                        row['accent'])
+            i += 1
+        print(sqlInsert)
+        sqlInsert = sqlInsert.replace("''", "NULL")
+        cursor.execute(sqlInsert)
+        conn.commit()
+
+# def Main():
+#     (curs, con) = Connect(configPath)
+#     InsertFromCSV(curs,con,'Files/cv-valid-train.csv')
+#     Disconnect(con,curs)
+# Main()
